c4b1b68c15f32265113688fbfff59d624182b411
mspronesti <massimiliano.pronesti@gmail.com>
2022-08-19 23:41:42 +0200

    fix(utils): missing imports in __init__ package

--- mlprlib/utils/__init__.py
+++ mlprlib/utils/__init__.py
@@ -1,3 +1,13 @@
+from .probability import *
 from .writer import Writer
 
-__all__ = ["Writer"]
+__all__ = [
+    "Writer",
+    "mean_squared_error",
+    "covariance_matrix",
+    "within_class_covariance",
+    "between_class_covariance",
+    "normal_pdf",
+    "normal_logpdf",
+    "multivariate_normal_logpdf"
+]
